e767f43a9785e6af22b4ea31a7468e2908da3d09
Alan <openxw@qq.com>
2019-07-07 21:07:41 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,22 @@
 <html>
 <head>
   <title>@yield('title','CDITD.com') - 酷龙学院</title>
+  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 <body>
-  @yield('content')
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a href="navbar-brand" href="/">酷龙学院</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 </html>
--- resources/views/staic_pages/home.blade.php
+++ resources/views/staic_pages/home.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.default')
-@section('title')
 
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>您好,学员们!</h1>
+    <p class="lead">
+      你现在看到的是酷龙学院的项目主页
+    </p>
+    <p>
+      一切,将从这里开始.
+    </p>
+    <p>
+      <a href="#" class="btn btn-lg btn-success" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
