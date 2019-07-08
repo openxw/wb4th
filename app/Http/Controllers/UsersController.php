ee64aeb321c48fef017f6021a3ff7f6e2fe9ce9c
Alan <openxw@qq.com>
2019-07-08 15:37:25 +0800

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,9 +25,10 @@ class UsersController extends Controller
         # code...
         $this->validate($request,[
             'name' => 'required|max:50',
-            'email' => 'required|email|unique:user|max:255',
+            'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
-        ]);
-        return;
+
+    ]);
+    return;
     }
 }
