666cc6abd2e53acd325c6b869de73d2cbe7493fc
Alan <openxw@qq.com>
2019-07-12 18:00:16 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth',[
-            'except' => ['show','create','store']
+            'except' => ['show','create','store','index']
             ]);
 
         $this->middleware('guest',[
@@ -75,5 +75,13 @@ class UsersController extends Controller
 
         return redirect()->route('users.show',$user->id);
     }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return view('users.index',compact('users'));
+    }
+
 
 }
