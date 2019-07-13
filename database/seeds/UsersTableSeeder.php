1e47133cff95b324a91d8abad9c3fad237c16e7c
Alan <openxw@qq.com>
2019-07-13 11:32:31 +0800

    移除email的form项

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,8 +11,8 @@ class UsersTableSeeder extends Seeder
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = '船长';
+        $user->email = 'xw@cditd.com';
         $user->is_admin = true;
         $user->save();
     }
